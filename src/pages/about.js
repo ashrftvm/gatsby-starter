30fb4f803bbc35baaccbbbbccb1e42ec59f553b5
Ashrf Ali <ashrftvm1@gmail.com>
2019-10-12 13:26:50 +0400

    added about and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
+        quia ullam possimus enim odio laboriosam tempore sint aspernatur atque
+        maxime!
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
+        quia ullam possimus enim odio laboriosam tempore sint aspernatur atque
+        maxime!
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
