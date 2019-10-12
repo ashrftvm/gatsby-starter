4ba6999e4f1b89b815e0263f141f9f03ef91b2fc
Ashrf Ali <ashrftvm1@gmail.com>
2019-10-12 13:21:18 +0400

    pages with jsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>
+        I am Asif, Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        obcaecati.
+      </h2>
+    </div>
+  )
+}
+
+export default IndexPage
